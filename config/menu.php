c038a90c2c4c93947a3ce478b6f8b3f16e930955
heshaohua <hesh.liucuan@gmail.com>
2019-06-26 14:28:48 +0800

    menu update

--- config/menu.php
+++ config/menu.php
@@ -32,6 +32,81 @@ return [
                     ]
                 ]
             ]
+        ],
+        'category'=>['label'=>'Category & Tags','items'=>[
+                ['label'=>'Category',
+                    'items' => [
+                        ['label'=>'Category List','url'=>['/category/normal']]
+                    ]
+                ],
+                ['label'=>'Category For Articles',
+                    'items' => [
+                        ['label'=>'Category List','url'=>['/category/article']]
+                    ]
+                ],
+                ['label'=>'Tags',
+                    'items' => [
+                        ['label'=>'Tags List','url'=>['/tag/index']],
+                        ['label'=>'Import CSV','url'=>['/tag/import']]
+                    ]
+                ]
+            ]
+        ],
+        'article'=>['label'=>'Articles','items'=>[
+                ['label'=>'Articles',
+                    'items' => [
+                        ['label'=>'Articles List','url'=>['/article/index']],
+                        ['label'=>'Add Article','url'=>['/article/add']],
+                        ['label'=>'Draft List','url'=>['/article/draft']],
+                        ['label'=>'Review List','url'=>['/article/review']]
+                    ]
+                ]
+            ]
+        ],
+        'theme'=>['label'=>'Themespack','items'=>[
+                ['label'=>'Themespack',
+                    'items' => [
+                        ['label'=>'Themespack List ','url'=>['/theme/index']],
+                        ['label'=>'Add Themespack','url'=>['/theme/add']],
+                        ['label'=>'Draft List ','url'=>['/theme/draft']],
+                        ['label'=>'Review List','url'=>['/theme/review']]
+                    ]
+                ]
+            ]
+        ],
+        'wallpaper'=>['label'=>'Wallpapers','items'=>[
+                ['label'=>'Wallpapers',
+                    'items' => [
+                        ['label'=>'Wallpapers List','url'=>['/wallpaper/index']],
+                        ['label'=>'Add Wallpapers','url'=>['/wallpaper/add']],
+                        ['label'=>'Draft List ','url'=>['/wallpaper/draft']],
+                        ['label'=>'Review List','url'=>['/wallpaper/review']]
+                    ]
+                ]
+            ]
+        ],
+        'collection'=>['label'=>'Wallpaper Collection','items'=>[
+                ['label'=>'Wallpaper Collection',
+                    'items' => [
+                        ['label'=>'Collections List','url'=>['/collection/index']],
+                        ['label'=>'Add Collections','url'=>['/collection/add']],
+                        ['label'=>'Draft List ','url'=>['/collection/draft']],
+                        ['label'=>'Review List','url'=>['/collection/review']]
+                    ]
+                ]
+            ]
+        ],
+        'app'=>['label'=>'Theme Apps','items'=>[
+                ['label'=>'Theme Apps',
+                    'items' => [
+                        ['label'=>'Apps List','url'=>['/app/index']],
+                        ['label'=>'Add App','url'=>['/app/add']],
+                        ['label'=>'Crawl App','url'=>['/app/crawl']],
+                        ['label'=>'Draft List ','url'=>['/app/draft']],
+                        ['label'=>'Review List','url'=>['/app/review']]
+                    ]
+                ]
+            ]
         ]
     ]
 ];
